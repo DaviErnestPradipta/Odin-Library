742fa837f1acdd6db350bc5e2d14e7201fb8206f
DaviErnestPradipta <daviernestpradipta@gmail.com>
2025-04-27 20:27:39 +0900

    Modify form toggle behavior

--- script.js
+++ script.js
@@ -9,17 +9,45 @@ form.addEventListener('submit', (e) => {
     const pages = document.getElementById('pages').value.trim();
     const isRead = document.getElementById('is-read').checked;
 
-    // Create a new book card
     const card = document.createElement('div');
     card.classList.add('book-card');
-    
-    card.innerHTML = `
-        <h3>"${title}"</h3>
-        <p><strong>Author: </strong> ${author}</p>
-        <p><strong>Pages: </strong> ${pages}</p>
-        <p><strong>Status: </strong> ${isRead ? 'Read' : 'Not Read Yet'}</p>
-    `;
 
+    const bookTitle = document.createElement('h3');
+    bookTitle.textContent = `"${title}"`;
+
+    const bookAuthor = document.createElement('p');
+    bookAuthor.innerHTML = `<strong>Author: </strong> ${author}`;
+
+    const bookPages = document.createElement('p');
+    bookPages.innerHTML = `<strong>Pages: </strong> ${pages}`;
+
+    const bookStatus = document.createElement('p');
+    bookStatus.innerHTML = `<strong>Status: </strong> ${isRead ? 'Yes' : 'No'}`;
+
+    const toggleButton = document.createElement('button');
+    toggleButton.textContent = 'Toggle';
+    toggleButton.addEventListener('click', () => {
+        bookStatus.innerHTML = `<strong>Status: </strong> 
+        ${(bookStatus.innerHTML.includes('Yes')) ? 'No' : 'Yes'}`
+    });
+
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = 'Delete';
+    deleteButton.addEventListener('click', () => {
+        card.remove();
+    });
+
+    // Add everything into the card
+    card.appendChild(bookTitle);
+    card.appendChild(bookAuthor);
+    card.appendChild(bookPages);
+    card.appendChild(bookStatus);
+    card.appendChild(toggleButton);
+    card.appendChild(deleteButton);
+
+    // Add the card to the grid
     bookGrid.appendChild(card);
+
+    // Reset the form
     form.reset();
 });
